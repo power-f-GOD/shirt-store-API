6f342aec548e0f69ba9cc2b7ee9526c53bf86566
power-f-god <sundaypower9@gmail.com>
2023-03-03 15:40:27 +0100

    feat: Init NestJS app.

---
+++ .eslintrc.cjs
@@ -0,0 +1,73 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: 'tsconfig.json',
+    tsconfigRootDir: __dirname,
+    sourceType: 'module'
+  },
+  plugins: ['@typescript-eslint/eslint-plugin'],
+  extends: [
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended'
+  ],
+  root: true,
+  env: {
+    node: true,
+    jest: true
+  },
+  ignorePatterns: [
+    'node_modules/*',
+    'dist/*',
+    '!prettier.config.js',
+    '.eslintrc.cjs',
+    '*.min.js'
+  ],
+  rules: {
+    '@typescript-eslint/interface-name-prefix': 'off',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+    'comma-dangle': 'error',
+    quotes: [
+      'error',
+      'single',
+      { avoidEscape: true, allowTemplateLiterals: true }
+    ],
+    curly: ['error', 'multi-line'],
+    'no-await-in-loop': 'error',
+    'no-constant-binary-expression': 'error',
+    'no-duplicate-imports': 'error',
+    'no-promise-executor-return': 'error',
+    'no-self-compare': 'error',
+    eqeqeq: 'error',
+    'prettier/prettier': ['error', {}, { usePrettierrc: true }], // Includes .prettierrc.js rules
+    // We will use TypeScript's types for component props instead
+    'react/prop-types': 'off',
+
+    // No need to import React when using Next.js
+    'react/react-in-jsx-scope': 'off',
+
+    // This rule is not compatible with Next.js's <Link /> components
+    'jsx-a11y/anchor-is-valid': 'off',
+
+    // Why would you want unused vars?
+    '@typescript-eslint/no-unused-vars': ['error'],
+
+    '@typescript-eslint/no-non-null-assertion': 'off',
+
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+
+    '@typescript-eslint/no-explicit-any': 'off',
+
+    'jsx-a11y/click-events-have-key-events': 'off',
+
+    // I suggest this setting for requiring return types on functions only where useful
+    '@typescript-eslint/explicit-function-return-type': [
+      'off',
+      {
+        allowExpressions: true,
+        allowConciseArrowFunctionExpressionsStartingWithVoid: true
+      }
+    ]
+  }
+};
